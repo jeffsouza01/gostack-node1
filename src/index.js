ff3909ea39aa9e32f31cb1b18096982bdd948957
Jefferson da Silva <je_jss@hotmail.com>
2020-07-13 02:57:41 -0300

    Update routes on http methods

--- src/index.js
+++ src/index.js
@@ -1,53 +1,63 @@
 const express = require('express');
-const { response } = require('express');
+const {uuid} = require('uuidv4')
 
 const app = express();
 
 app.use(express.json());
 
+
+const projects = [];
+
 app.get('/projects', (req, res) => {
-    const {title, owner} = req.query;
+    //const {title, owner} = req.query;
 
-    console.log(`${title} e ${owner}`);
-
-    return res.json([
-        'Project 1',
-        'Project 2',
-        'Project 3'
-    ]);
+    return res.json(projects);
 })
 
 app.post('/projects', (req, res) => {
-    const body = req.body;
+    const {title, owner } = req.body
+    const project = { id: uuid(), title, owner};
 
-    console.log(body);
-
-    return res.json([
-        'Project 4',
-        'Project 5',
-        'Project 6'
-    ])
+    projects.push(project);
+    return res.json(project)
 })
 
 app.put('/projects/:id', (req, res) => {
     const {id} = req.params;
+    const { title, owner} = req.body;
 
-    console.log(id);
+    const projectIndex = projects.findIndex(project => project.id === id);
+
     
-    return res.json([
-        'Project 6',
-    ])
+    if (projectIndex < 0) {
+        return res.status(400).json({ error: "Project not found!"})
+    }
+
+    const project = {
+        id,
+        title,
+        owner
+    };
+
+    projects[projectIndex] = project;
+    
+    return res.json(project);
 })
 
 
 app.delete('/projects/:id', (req, res) => {
     const {id} = req.params;
 
-    console.log(id);
+    const projectIndex = projects.findIndex(project => project.id === id);
 
-    return res.json([
-        'Project 2'
-    ])
+    
+    if (projectIndex < 0) {
+        return res.status(400).json({ error: "Project not found!"});
+    }
+
+    projects.splice(projectIndex, 1);
+
+    return res.status(200).json({ message: "Project Deleted"});
 })
 
 app.listen(3000, () => console.log('Server Started!'));
